b58ceade1ad0f6a08c528a92b514f260e306d1ec
George Fonseca <george.fons@gmail.com>
2022-08-27 01:19:55 +0000

    Aula prática III - parte II

--- Main.java
+++ Main.java
@@ -1,19 +1,26 @@
 class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
-    Graph g1 = new Graph(4);
-    g1.addEdge(0, 1, 1);
-    g1.addEdge(1, 0, 1);
-    g1.addEdge(0, 3, 1);
-    g1.addEdge(3, 0, 1);
-    System.out.println(g1);
-    g1.degree(0); // 2
-    g1.degree(1); // 1
-    g1.degree(2); // 0
-    g1.degree(3); // 1
-    System.out.println(g1.highestDegree()); // 2
-    System.out.println(g1.lowestDegree()); // 0
-    System.out.println(g1.complement());
-    System.out.println(g1.oriented());
+    Graph g1 = new Graph(8);
+    g1.addEdgeUnoriented(6, 3, 1);
+    g1.addEdgeUnoriented(6, 7, 1);
+    g1.addEdgeUnoriented(6, 1, 1);
+    g1.addEdgeUnoriented(3, 4, 1);
+    g1.addEdgeUnoriented(7, 0, 1);
+    g1.addEdgeUnoriented(1, 2, 1);
+    g1.addEdgeUnoriented(0, 5, 1);
+    System.out.println(g1.dfs(2));
+    // g1.addEdge(0, 1, 1);
+    // g1.addEdge(1, 0, 1);
+    // g1.addEdge(0, 3, 1);
+    // g1.addEdge(3, 0, 1);
+    // System.out.println(g1);
+    // g1.degree(0); // 2
+    // g1.degree(1); // 1
+    // g1.degree(2); // 0
+    // g1.degree(3); // 1
+    // System.out.println(g1.highestDegree()); // 2
+    // System.out.println(g1.lowestDegree()); // 0
+    // System.out.println(g1.complement());
+    // System.out.println(g1.oriented());
   }
 }
